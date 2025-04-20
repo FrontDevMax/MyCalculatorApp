3b54bd34eb5ecc6d042a50b1f87e51e50a01d7ef
FrontDevMax <snipergame2006@gmail.com>
2025-04-20 17:08:49 +0600

    The app design is complete

--- CalculatorApp/app/src/main/java/com/example/calculatorapp/LoginActivity.java
+++ CalculatorApp/app/src/main/java/com/example/calculatorapp/LoginActivity.java
@@ -1,8 +1,15 @@
 package com.example.calculatorapp;
 
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.content.Intent;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,11 +20,6 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
-        linkTextRegistration = findViewById(R.id.linkTextRegistration);
-        linkTextRegistration.setOnClickListener(v -> {
-            Intent intent = new Intent(this, RegistrationActivity.class);
-            startActivity(intent);
-        });
     }
     public void showCalculator(View view) {
         Intent intent = new Intent(this, CalculatorActivity.class);
